a814561f1d219d5f77730611ff60ffa30fe248d1
zkaakakag <cscom222@naver.com>
2025-02-03 17:30:53 +0900

    Remove deleted files

--- react/basic/greetingClass.js
+++
@@ -1,8 +0,0 @@
-class Greeting extends React.Component {
-  render() {
-    return <h1 className="greeting">Hello World1~</h1>;
-  }
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(React.createElement(Greeting));
--- react/basic/greetingFunc.js
+++
@@ -1,6 +0,0 @@
-function Greeting() {
-  return <h1 className="greeting">Hello World2~</h1>;
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(React.createElement(Greeting));
