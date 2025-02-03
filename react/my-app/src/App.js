faeb033c87bec918826aca2336675f8200186422
zkaakakag <cscom222@naver.com>
2025-02-03 17:35:51 +0900

    init: React project setup

---
+++ react/my-app/src/App.js
@@ -0,0 +1,6 @@
+import "./App.css";
+function App() {
+  return <h1 className="App">Hello React App~~</h1>;
+}
+
+export default App;
---
+++ react/my-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; //client: FSR server:SSR(next.js)
+import App from "./App1";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
