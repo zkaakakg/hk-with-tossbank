2451923354edadbd6e1c370f694367df988bb968
zkaakakag <cscom222@naver.com>
2025-02-04 16:26:11 +0900

    Update index.js

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; //client: FSR server:SSR(next.js)
-import App from "./App1";
+import App from "./App4";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
