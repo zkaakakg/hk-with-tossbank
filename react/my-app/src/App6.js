b4bac15f9ab5ed8ed06b76b77cdd93478e334df2
zkaakakag <cscom222@naver.com>
2025-02-05 17:48:35 +0900

    App6.js 수정

--- react/my-app/src/App6.js
+++ react/my-app/src/App6.js
@@ -2,7 +2,8 @@ import React from "react";
 import * as E from "./6-state/State";
 
 function App() {
-  return <E.LikeButton />;
+  // return <E.LikeButton />;
+  return <E.Counter />;
 }
 
 export default App;
