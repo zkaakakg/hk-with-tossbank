4dd6699722becb28c93e83fadc4ac2bc420bbb46
zkaakakag <cscom222@naver.com>
2025-02-06 17:34:04 +0900

    Create App9.js

---
+++ react/my-app/src/App9.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import * as E from "./9-conditionalRender/ConditionalRender";
+import * as ex from "./9-conditionalRender/Ex";
+
+function App() {
+  return <ex.Conditional3 />;
+}
+
+export default App;
