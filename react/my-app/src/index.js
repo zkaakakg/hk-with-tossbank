78f7abd38ca11e57d0f62bfdad73b989a3c0585a
zkaakakag <cscom222@naver.com>
2025-02-06 17:33:51 +0900

    index.js 수정

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; //client: FSR server:SSR(next.js)
-import App from "./App6";
+import App from "./App9";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
