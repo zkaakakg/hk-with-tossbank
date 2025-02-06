1f1598bf49ad9aba9a632af437eeabf19d97ace0
zkaakakag <cscom222@naver.com>
2025-02-06 17:33:56 +0900

    Create App7.js

---
+++ react/my-app/src/App7.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import * as E from "./7-hook/Hook";
+// import * as ex from "./7-hook/Ex";
+import Counter from "./7-hook/CustomHook";
+
+function App() {
+  return <Counter />;
+}
+
+export default App;
