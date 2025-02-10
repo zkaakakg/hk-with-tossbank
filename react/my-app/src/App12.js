4c940b68363cbfbaad72bee5d71273070b89226f
zkaakakag <cscom222@naver.com>
2025-02-10 17:31:28 +0900

    App12.js 생성

---
+++ react/my-app/src/App12.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import * as E from "./12-shared-state/LiftingStateUP";
+import * as ex from "./12-shared-state/Ex";
+
+function App() {
+  return <ex.SelectedList />;
+}
+
+export default App;
