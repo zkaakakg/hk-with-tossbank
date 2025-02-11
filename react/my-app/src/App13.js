9fa3bb260a0ccf950d0a66010738d3f7e5aec4a8
zkaakakag <cscom222@naver.com>
2025-02-11 10:17:10 +0900

    App13.js 수정

---
+++ react/my-app/src/App13.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import StyledComponent1 from "./13-style/Style";
+// import StyledComponent1 from "./13-style/StyledComponent1";
+// import StyledComponent2 from "./13-style/StyledComponent2";
+import StyledComponent3 from "./13-style/StyledComponent3";
+
+function App() {
+  return <StyledComponent3 />;
+}
+
+export default App;
