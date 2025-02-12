fba5548ca83a608fa97286607b7b9568bbf91047
zkaakakag <cscom222@naver.com>
2025-02-12 14:31:22 +0900

    App14.js 생성

---
+++ react/my-app/src/App14.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import ThemeContextUseContext from "./14-context/ThemeContextUseContext";
+import ThemeContextProvider from "./14-context/ThemeContextProvider";
+
+function App() {
+  return <ThemeContextProvider intitTheme="dark" />;
+}
+
+export default App;
