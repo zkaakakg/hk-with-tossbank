29fd453e9409b6aa98d0fa1a63e342f963883fe1
zkaakakag <cscom222@naver.com>
2025-02-12 11:26:40 +0900

    App13.jsx 수정

--- react/my-app/src/App13.js
+++ react/my-app/src/App13.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import StyledComponent1 from "./13-style/Style";
+// import InlineStyle from "./13-style/Style";
 // import StyledComponent1 from "./13-style/StyledComponent1";
 // import StyledComponent2 from "./13-style/StyledComponent2";
 import StyledComponent3 from "./13-style/StyledComponent3";
