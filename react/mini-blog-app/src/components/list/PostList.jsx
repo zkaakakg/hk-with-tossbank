689e7b5dab55d2a1bbf7e741df125cf48c95b1a0
zkaakakag <cscom222@naver.com>
2025-02-14 15:37:33 +0900

    글 목록

---
+++ react/mini-blog-app/src/components/list/PostList.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import PostListItem from "./PostListItem";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 10px;
+  /* & : 부모선택자 scss/sass 문법 */
+  & > * {
+    :not(:last-child) {
+      margin-bottom: 16px;
+    }
+  }
+`;
+
+function PostList(props) {
+  const { posts, onClickItem } = props;
+
+  return (
+    <Wrapper>
+      {posts.map((post, index) => {
+        return (
+          <PostListItem
+            key={post.id}
+            post={post}
+            onClick={() => {
+              onClickItem(post); //navigete : 페이지이동을 한다.
+            }}
+          />
+        );
+      })}
+    </Wrapper>
+  );
+}
+
+export default PostList;
