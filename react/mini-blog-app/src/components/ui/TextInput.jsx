08d18a7f863bbef4552ceb4c2e0cd5da2b154e32
zkaakakag <cscom222@naver.com>
2025-02-17 11:04:28 +0900

    TextInput.jsx 수정

--- react/mini-blog-app/src/components/ui/TextInput.jsx
+++ react/mini-blog-app/src/components/ui/TextInput.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledTextarea = styled.textarea`
-  width: calc(100% -32px);
+  width: calc(100% - 32px);
   ${(props) => props.height && `height: ${props.height}px;`}
   padding: 16px;
   font-size: 16px;
