3451e13dca5efd795700fce7ac7bbbfa29cb73c0
zkaakakag <cscom222@naver.com>
2025-02-21 15:08:08 +0900

    CRUD 구현 (mongoDB연동)

--- nodejs/basics/ex19-blog-server.js
+++ nodejs/basics/ex19-blog-server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 // MongoDB Atlas 연결
 const MONGO_URI =
-  'mongodb+srv://admin:1234@mongodb-cluster.wo3pk.mongodb.net/blog'
+  'mongodb+srv://haewon:1234@mongodb-cluster.y8er1.mongodb.net/blog'
 mongoose
   .connect(MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
