0d41c6c0b2c3694735478507732e7950e33b67bd
zkaakakag <cscom222@naver.com>
2025-02-24 16:12:14 +0900

    typescript 타입 별칭

---
+++ typescript/src/ex04.ts
@@ -0,0 +1,27 @@
+// 타입 별칭(Type Alias)
+type User = {
+  id: number
+  name: string
+  nicname: string
+  birth: string
+  location: string
+  phone: string
+}
+
+let user1: User = {
+  id: 1,
+  name: '홍길동',
+  nicname: 'hong',
+  birth: '2000.01.01',
+  location: '한양',
+  phone: '010-1111-2222',
+}
+
+let user2: User = {
+  id: 2,
+  name: '사임당',
+  nicname: 'dang',
+  birth: '2000.01.01',
+  location: '한양',
+  phone: '010-1111-2222',
+}
