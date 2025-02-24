34303f3cd8b2a5deb6e54be472e1ea1a2ff6979e
zkaakakag <cscom222@naver.com>
2025-02-24 16:11:10 +0900

    typescript 기본타입

---
+++ typescript/src/ex01.ts
@@ -0,0 +1,27 @@
+// 기본타입(primitive type) : 하나의 갑만 가지는 타입
+
+// 명시적 타입 선언 vs 암묵적 타입선언
+// let myNun1: number = 123
+// let myNum2 = 123
+
+//number
+let num1: number = 123
+
+// string
+let str1: string = 'hello'
+
+// boolean
+let bool1: boolean = true
+
+// null
+let null1: null = null
+
+// undefined
+let unde1: undefined = undefined
+
+// 리터럴 타입
+let lit1: 10 = 10
+
+//  "strictNullChecks": true
+// null값을 변수에 할당하지 못하도록 한다.
+let num1A: number = null
