2b3f175b0a6804b21227abf234f3b9f694ae5207
zkaakakag <cscom222@naver.com>
2025-02-25 13:27:03 +0900

    typescript 타입별칭

--- typescript/src/ex04.ts
+++ typescript/src/ex04.ts
@@ -25,3 +25,34 @@ let user2: User = {
   location: '한양',
   phone: '010-1111-2222',
 }
+
+// 객체 리터럴
+type CountryCodes = {
+  Korea: string
+  UnitedState: string
+  UnitedKingdom: string
+}
+
+let countryCodes: CountryCodes = {
+  Korea: 'ko',
+  UnitedState: 'us',
+  UnitedKingdom: 'uk',
+}
+//인덱스 시그니처
+type CountryCodes2 = {
+  [key: string]: string
+}
+let countryCodes2: CountryCodes2 = {
+  Korea: 'ko',
+  UnitedState: 'us',
+  UnitedKingdom: 'uk',
+}
+
+type CountryCodes3 = {
+  [key: string]: string
+  korea: 'ko' // koerea는 꼭 있어야 함
+}
+
+let countryCodes3: CountryCodes3 = {
+  korea: 'ko', // korea는 꼭 있어야함
+}
