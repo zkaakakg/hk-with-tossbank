88d188463be9607bd46fcafedfced8275f88a452
zkaakakag <cscom222@naver.com>
2025-02-27 17:10:25 +0900

    Button UI

---
+++ react/mini-blog-db-typescript/src/components/ui/Button.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  padding: 8px 16px;
+  font-size: 20px;
+  border-width: 1px;
+  border-radius: 8px;
+  cursor: pointer;
+`;
+
+interface ButtonProps {
+  title?: string;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ title = "button", onClick }) => {
+  return <StyledButton onClick={onClick}>{title}</StyledButton>;
+};
+
+export default Button;
