f58cbca4474705f0f27c27719835d6c29a6ead22
zkaakakag <cscom222@naver.com>
2025-03-20 11:14:02 +0900

    집합연산자/IS NULL/IN/BETWEEN...AND/LIKE/

--- sql/ch02.sql
+++ sql/ch02.sql
@@ -65,6 +65,80 @@ SELECT *
 FROM 고객
 WHERE 도시 = '부산광역시' AND 마일리지 < 1000;
 
+-- 집합 연산자 UNION, UNION ALL
+-- UNION : 두 개의 SELECT 결과를 합쳐 줌
+--  	 : 컬럼의 갯수와 타입을 일치시켜야 됨
+-- UNION ALL : 중복되는 레코드도 다 출력해줌
+USE 세계무역;
+
+SELECT 고객번호, 도시, 마일리지
+FROM 고객
+WHERE 도시 = '부산광역시'
+UNION 
+SELECT 고객번호, 도시, 마일리지
+FROM 고객
+WHERE 마일리지 < 1000
+ORDER BY 고객번호;
+
+-- IS NULL : 값이 NULL이면 TURE, 아니면 FALSE를 반환하는 연산자
+SELECT * FROM 고객
+ WHERE 지역 IS NOT NULL;
+
+SELECT * 
+FROM 고객
+WHERE 지역 IS NULL;
+
+SELECT *
+FROM 고객
+WHERE 지역 = '';
+
+-- IN : ~ 중에 하나가 있으면 true(여러 개의 or을 대체)
+SELECT 고객번호
+	  ,담당자명
+	  ,담당자직위
+FROM 고객
+WHERE 담당자직위 IN ('영업 과장', '마케팅 과장');
+-- BETWEEN AND : ~이상 ~이하 범위를 지정할 때(비교, AND를 대체)
+SELECT 담당자명
+	  ,마일리지
+FROM 고객
+WHERE 마일리지 BETWEEN 100000 AND 200000;
+
+
+-- LIKE : 문자열의 일부를 검사할 때 사용
+--      : % 여러 문자열을 대체
+--      : _ 한 글자를 대체
+SELECT *
+FROM 고객
+WHERE 도시 LIKE '%광역시'
+AND (고객번호 LIKE '_C%' OR 고객번호 LIKE '__c%');
+
+SELECT * FROM 제품;
+
+-- 실전문제
+-- 1. 제품 테이블에서 세계무역이 취급하는 제품 중에서 '주스' 제품에 대한 모든 정보를 검색하시오.
+SELECT * 
+FROM 제품
+WHERE 제품명 LIKE '%주스';
+
+-- 2. 제품 테이블에서 단가가 5,000원 이상 10,000원 이하인 '주스'제품에는 무엇이 있는지 검색하시오.
+SELECT *
+FROM 제품
+WHERE 단가 BETWEEN 5000 AND 10000
+AND (제품명 LIKE '%주스');
+
+-- 3. 제품 테이블에서 제품번호가 1,2,4,7,11,20인 제품의 모든 정보를 보이시오.
+SELECT * 
+FROM 제품
+WHERE 제품번호 IN (1,2,4,7,11,20);
+
+-- 4. 제품 테이블에서 재고금액이 높은 상위 10개 제품에 대해 제품번호, 제품명, 
+-- 단가, 재고, 재고금액(단가 * 재고)을 보이시오.
+SELECT 제품번호,제품명, 단가, 재고, 단가 * 재고 AS '재고금액'
+FROM 제품
+ORDER BY 재고금액 DESC
+LIMIT 10;
+
 -- 연습문제 DM 제출
 -- 1.'서울'에 사는 고객 중에 마일리지가 15,000점 이상 20,000점 이하인 고객의 
 --    모든 컬럼 정보를 보이시오.
