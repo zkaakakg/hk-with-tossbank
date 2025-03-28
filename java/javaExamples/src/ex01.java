164148a2439491c8f36c1ccf38e6ab33ec174978
zkaakakag <cscom222@naver.com>
2025-03-28 17:05:21 +0900

    자바 기본 개념

---
+++ java/javaExamples/src/ex01.java
@@ -0,0 +1,13 @@
+//자바는 클래스에서 시작해서 끝난다.
+//public : 접근제한자
+//class : 클래스 정의어
+public class ex01 {
+    //static : 정적 변수/함수로 선언
+    //       : 프로그램 시작점에 정의한다.
+    //String[] : 문자열 배열, 프로그램 시작시
+    //        파라미터 ex) hwp.exe a.hwp b.hwp
+    public static void main(String[] args) {
+        //println : 콘솔 출력
+        System.out.println("Hello Java!");
+    }
+}
