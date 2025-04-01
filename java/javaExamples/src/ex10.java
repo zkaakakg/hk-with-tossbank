d9500a11a71d3366078b631f67534bd54bc9feb7
zkaakakag <cscom222@naver.com>
2025-04-01 09:00:59 +0900

    단축키

---
+++ java/javaExamples/src/ex10.java
@@ -0,0 +1,29 @@
+public class ex10 {
+    public static void main(String[] args) {
+//      인텔리제이 단축키
+        System.out.println("sout");
+        int x = 10;
+//      soutv
+        System.out.println("x = " + x);
+
+        myFunc(20);
+
+//      fori
+        for (int i = 0; i < 5 ; i++) {
+            System.out.println(i);
+        }
+
+        int[] array = {};
+//      ifn
+        if (array == null) {
+
+        }
+
+    }
+    static void myFunc(int p){
+//      sout
+        System.out.println("ex10.myFunc");
+//      soutp
+        System.out.println("p = " + p);
+    }
+}
