870aac33ed820f02f55b3a4c228ba385d33ed2fb
zkaakakag <cscom222@naver.com>
2025-04-02 16:46:32 +0900

    스프링 프로젝트 Ex03Autowired 생성

---
+++ springboot/Ex03Autowired/src/main/java/com/study/springboot/Ex03AutowiredApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex03AutowiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex03AutowiredApplication.class, args);
+	}
+
+}
