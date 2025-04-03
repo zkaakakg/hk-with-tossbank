ffafd717150ad2e4637f8c2d2a56b32b93a587dc
zkaakakag <cscom222@naver.com>
2025-04-03 15:24:03 +0900

    Ex04Lombok 프로젝트 생성

---
+++ springboot/Ex04Lombok/src/main/java/com/stydy/springboot/Ex04LombokApplication.java
@@ -0,0 +1,13 @@
+package com.stydy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex04LombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex04LombokApplication.class, args);
+	}
+
+}
---
+++ springboot/Ex04Lombok/src/test/java/com/stydy/springboot/Ex04LombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stydy.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex04LombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
