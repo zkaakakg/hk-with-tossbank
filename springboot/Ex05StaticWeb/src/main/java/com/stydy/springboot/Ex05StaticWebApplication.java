e21ccb2d91e0f0bba6f1925d6549602575a80b01
zkaakakag <cscom222@naver.com>
2025-04-03 15:25:08 +0900

    Ex05StaticWeb 프로젝트 생성

---
+++ springboot/Ex05StaticWeb/src/main/java/com/stydy/springboot/Ex05StaticWebApplication.java
@@ -0,0 +1,13 @@
+package com.stydy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex05StaticWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex05StaticWebApplication.class, args);
+	}
+
+}
