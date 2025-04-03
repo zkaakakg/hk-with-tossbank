ab1352c63b0b71c68ec577c76c5d683122a83459
zkaakakag <cscom222@naver.com>
2025-04-03 15:25:08 +0900

    Ex05StaticWeb 프로젝트 생성

---
+++ springboot/Ex05StaticWeb/src/test/java/com/stydy/springboot/Ex05StaticWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stydy.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex05StaticWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
