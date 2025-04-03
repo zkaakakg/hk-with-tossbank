221ef85eef8cbe0e11189636ab4925079acf85b2
zkaakakag <cscom222@naver.com>
2025-04-03 15:26:23 +0900

    Ex06Thymefleaf 프로젝트 생성

---
+++ springboot/Ex06Thymefleaf/src/main/java/com/study/springboot/Ex06ThymefleafApplication.java
@@ -0,0 +1,12 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex06ThymefleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex06ThymefleafApplication.class, args);
+	}
+}
