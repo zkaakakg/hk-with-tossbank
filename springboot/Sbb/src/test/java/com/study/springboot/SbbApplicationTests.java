bcff9331ed87cfa0770fb783cffe69c76abb4679
zkaakakag <cscom222@naver.com>
2025-04-08 08:56:53 +0900

    Sbb 프로젝트 생성

---
+++ springboot/Sbb/src/test/java/com/study/springboot/SbbApplicationTests.java
@@ -0,0 +1,26 @@
+package com.study.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Test
+	void testJpa() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
+	}
+
+}
