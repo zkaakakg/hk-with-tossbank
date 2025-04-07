7da05c22fc5d455ab7f1ca2dac5289cfd83ebd68
zkaakakag <cscom222@naver.com>
2025-04-07 18:07:46 +0900

    Ex11H2Db 프로젝트 생성

---
+++ springboot/Ex11H2DB/src/main/java/com/study/springboot/Ex11H2DbApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex11H2DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex11H2DbApplication.class, args);
+	}
+
+}
