e75ffed2706563847874dea3462f67c456e51366
zkaakakag <cscom222@naver.com>
2025-04-08 17:24:31 +0900

    질문 생성/목록/상세보기 로직

---
+++ springboot/Sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -0,0 +1,44 @@
+package com.mysite.sbb.question;
+
+import com.mysite.sbb.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public Page<Question> getList(int page){
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page,10,Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        }else{
+            throw new DataNotFoundException("question not found");
+        }
+    }
+
+    public void create(String subject, String content){
+        Question question = new Question();
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(question);
+    }
+}
