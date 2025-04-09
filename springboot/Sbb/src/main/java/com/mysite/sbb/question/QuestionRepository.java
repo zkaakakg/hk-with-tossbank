94314f7b116900cfa33916aac3ca798e6dea619f
zkaakakag <cscom222@naver.com>
2025-04-09 17:39:07 +0900

    검색기능을 위한 findAll 메서드 추가

--- springboot/Sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ springboot/Sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +14,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
     Page<Question> findAll(Pageable pageable);
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
