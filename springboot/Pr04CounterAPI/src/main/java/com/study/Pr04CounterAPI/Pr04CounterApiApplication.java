08bb448583bc93a4d48eabf7ac44f39563dede74
zkaakakag <cscom222@naver.com>
2025-04-09 17:32:33 +0900

    Pr04CounterApi 프로젝트 생성

---
+++ springboot/Pr04CounterAPI/src/main/java/com/study/Pr04CounterAPI/Pr04CounterApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.Pr04CounterAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr04CounterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr04CounterApiApplication.class, args);
+	}
+
+}
---
+++ springboot/Pr04CounterAPI/src/test/java/com/study/Pr04CounterAPI/Pr04CounterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Pr04CounterAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr04CounterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
