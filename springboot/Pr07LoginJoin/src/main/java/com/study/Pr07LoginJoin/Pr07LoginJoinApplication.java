7f505326661628471eca864bb5bb23a44fe64ec5
zkaakakag <cscom222@naver.com>
2025-04-09 17:35:38 +0900

    Pr07LoginJoin 프로젝트 생성

---
+++ springboot/Pr07LoginJoin/src/main/java/com/study/Pr07LoginJoin/Pr07LoginJoinApplication.java
@@ -0,0 +1,13 @@
+package com.study.Pr07LoginJoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr07LoginJoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr07LoginJoinApplication.class, args);
+	}
+
+}
