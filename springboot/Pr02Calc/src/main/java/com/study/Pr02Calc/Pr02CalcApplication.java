7128ef90d25636649ceaae04d3b70d6d8ff6d158
zkaakakag <cscom222@naver.com>
2025-04-09 17:27:49 +0900

    Pr02Calc 프로젝트 생성

---
+++ springboot/Pr02Calc/src/main/java/com/study/Pr02Calc/Pr02CalcApplication.java
@@ -0,0 +1,13 @@
+package com.study.Pr02Calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr02CalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr02CalcApplication.class, args);
+	}
+
+}
---
+++ springboot/Pr02Calc/src/test/java/com/study/Pr02Calc/Pr02CalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Pr02Calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr02CalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
