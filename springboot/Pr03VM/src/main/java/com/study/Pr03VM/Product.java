532a47681af7c9132f2b577e004c0661a383cc7f
zkaakakag <cscom222@naver.com>
2025-04-09 17:30:26 +0900

    상품 클래스

---
+++ springboot/Pr03VM/src/main/java/com/study/Pr03VM/Product.java
@@ -0,0 +1,17 @@
+package com.study.Pr03VM;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String name; // 상품명
+    private int price; // 가격
+    private LocalDate limit_date; // 유통기한
+}
