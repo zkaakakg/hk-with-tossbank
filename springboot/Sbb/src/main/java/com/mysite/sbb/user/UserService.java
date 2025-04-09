f93c9cfddc94e6e40ec3a8e8797f6643c27a3500
zkaakakag <cscom222@naver.com>
2025-04-09 17:41:11 +0900

    getUser() 로직 추가

--- springboot/Sbb/src/main/java/com/mysite/sbb/user/UserService.java
+++ springboot/Sbb/src/main/java/com/mysite/sbb/user/UserService.java
@@ -1,8 +1,11 @@
 package com.mysite.sbb.user;
 
+import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -18,5 +21,14 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
         return user;
+    }
+
+    public SiteUser getUser(String username){
+        Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
+        if (siteUser.isPresent()){
+            return siteUser.get();
+        }else{
+            throw new DataNotFoundException("siteuser not found");
+        }
     }
 }
