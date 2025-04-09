e0630eb19be7f5b63451363dd0f6f5fc965beac5
zkaakakag <cscom222@naver.com>
2025-04-09 17:38:17 +0900

    작성자 / 수정시간 추가

--- springboot/Sbb/src/main/java/com/mysite/sbb/question/Question.java
+++ springboot/Sbb/src/main/java/com/mysite/sbb/question/Question.java
@@ -1,12 +1,14 @@
 package com.mysite.sbb.question;
 
 import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.user.SiteUser;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -26,4 +28,12 @@ public class Question {
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
+
+    @ManyToOne
+    private SiteUser author;
+
+    private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
