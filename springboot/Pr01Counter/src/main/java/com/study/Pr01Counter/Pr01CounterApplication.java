5027974b8aefa4a9dec88d1fbd5eebde08ea537e
zkaakakag <cscom222@naver.com>
2025-04-09 17:25:43 +0900

    Pr01Counte 프로젝트 생성

---
+++ springboot/Pr01Counter/src/main/java/com/study/Pr01Counter/Pr01CounterApplication.java
@@ -0,0 +1,13 @@
+package com.study.Pr01Counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr01CounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr01CounterApplication.class, args);
+	}
+
+}
---
+++ springboot/Pr01Counter/src/test/java/com/study/Pr01Counter/Pr01CounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Pr01Counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr01CounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
