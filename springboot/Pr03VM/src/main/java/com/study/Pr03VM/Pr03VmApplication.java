8fe426dd5f8506201189e7c8ae08742e2446b899
zkaakakag <cscom222@naver.com>
2025-04-09 17:29:06 +0900

    Pr03Vm 프로젝트 생성

---
+++ springboot/Pr03VM/src/main/java/com/study/Pr03VM/Pr03VmApplication.java
@@ -0,0 +1,13 @@
+package com.study.Pr03VM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr03VmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr03VmApplication.class, args);
+	}
+
+}
---
+++ springboot/Pr03VM/src/test/java/com/study/Pr03VM/Pr03VmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Pr03VM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr03VmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
