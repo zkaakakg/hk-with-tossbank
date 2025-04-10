ec6a6ffeca4cc019dd66b863c41a8d906ba1a633
zkaakakag <cscom222@naver.com>
2025-04-10 17:43:30 +0900

    Ex12H2DbTest 프로젝트 생성

---
+++ springboot/Ex12H2DBTest/src/main/java/com/study/springboot/Ex12H2DbTestApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex12H2DbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex12H2DbTestApplication.class, args);
+	}
+
+}
