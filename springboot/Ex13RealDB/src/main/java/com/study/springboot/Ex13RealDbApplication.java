68ab579d29162c54b794e838c105b9079b696a14
gaeun.jang <cscom222@naver.com>
2025-04-13 22:36:56 +0900

    Ex13RealDB 프로젝트 생성

---
+++ springboot/Ex13RealDB/src/main/java/com/study/springboot/Ex13RealDbApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex13RealDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex13RealDbApplication.class, args);
+	}
+
+}
