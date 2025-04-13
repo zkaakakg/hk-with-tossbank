16230fd08fc576429a2828de4ed01ef44a94c90a
gaeun.jang <cscom222@naver.com>
2025-04-13 22:41:32 +0900

    Member Entity 수정

--- springboot/Ex12H2DBTest/src/main/java/com/study/springboot/MemberEntity.java
+++ springboot/Ex12H2DBTest/src/main/java/com/study/springboot/MemberEntity.java
@@ -1,38 +1,36 @@
 package com.study.springboot;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "member")
+@Table(name="member")
 @Getter
 //@Setter 비추천 - DB에 중복 적용됨.
-@Builder //객체 생성시 사용 - 생성자함수 대체
+@Builder  //객체생성시 사용 - 생성자함수 대체
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "user_id")
+    @Column(name="user_id")
     private String userId;
-    @Column(name = "user_pw")
+    @Column(name="user_pw")
     private String userPw;
-    @Column(name = "user_name")
+    @Column(name="user_name")
     private String userName;
-    @Column(name = "user_role")
+    @Column(name="user_role")
     private String userRole;
     @Column
     @DateTimeFormat(pattern = "yyyy-mm-dd")
     private LocalDate joindate;
 
-    public void update(String userId, String userPw, String userName, String userRole){
+    public void update(String userId, String userPw, String userName,
+                       String userRole){
         this.userId = userId;
         this.userPw = userPw;
         this.userName = userName;
