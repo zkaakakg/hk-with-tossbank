de6fcaafb22d812dc60ff1f8274cd3a5edb4943f
zkaakakag <cscom222@naver.com>
2025-04-14 17:35:15 +0900

    Users 클래스

---
+++ springboot/Ex15JSP/src/main/java/com/study/springboot/Users.java
@@ -0,0 +1,15 @@
+package com.study.springboot;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+    private int userNo;
+    private String userName;
+}
