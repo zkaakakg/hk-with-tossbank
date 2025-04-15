97781d2fdca86a24df66cb286de7b12cbfafb04d
zkaakakag <cscom222@naver.com>
2025-04-15 15:22:11 +0900

    Ex18LoginJoinDB 프로젝트 생성

---
+++ springboot/Ex18LoginJoinDB/src/main/java/com/study/springboot/Ex18LoginJoinDbApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex18LoginJoinDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex18LoginJoinDbApplication.class, args);
+	}
+
+}
