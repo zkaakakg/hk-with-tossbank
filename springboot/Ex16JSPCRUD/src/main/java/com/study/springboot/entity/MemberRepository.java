e573e96944bf0f6d7664bfae198d2b51babe9adf
zkaakakag <cscom222@naver.com>
2025-04-15 15:11:38 +0900

    MemberRepository

---
+++ springboot/Ex16JSPCRUD/src/main/java/com/study/springboot/entity/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.study.springboot.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    //JPA 기본함수
+    //1. findAll
+    //2. findById
+    //3. count()
+    //4. save()
+    //5. delete()
+}
