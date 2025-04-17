bf7ea537cfcfd5d939b79ed2c0e38afe6a057187
zkaakakag <cscom222@naver.com>
2025-04-17 15:45:19 +0900

    Member DTO

---
+++ springboot/Ex21TDD/src/main/java/com/study/springboot/MemberDto.java
@@ -0,0 +1,17 @@
+package com.study.springboot;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDto {
+    private String loginId;
+    private String loginPw;
+}
