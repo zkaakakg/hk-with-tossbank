f3e712551a381778d8e9bb65a7088b8836b9a2fb
zkaakakag <cscom222@naver.com>
2025-04-17 15:41:25 +0900

    사칙연산 계산기 클래스

---
+++ springboot/Ex21TDD/src/main/java/com/study/springboot/Calc.java
@@ -0,0 +1,20 @@
+package com.study.springboot;
+
+//사칙연산 계산기 클래스
+public class Calc {
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int sub(int x, int y) {
+        return x - y;
+    }
+
+    public int mul(int x, int y) {
+        return x * y;
+    }
+
+    public int div(int x, int y) {
+        return x / y;
+    }
+}
