0b5ab930dec94998df0aa32104698b793740af85
zkaakakag <cscom222@naver.com>
2025-04-24 09:37:06 +0900

    ReplySaveRequestDto  추가

---
+++ springboot/Ex24Board/src/main/java/com/study/springboot/dto/ReplySaveRequestDto.java
@@ -0,0 +1,31 @@
+package com.study.springboot.dto;
+
+import com.study.springboot.domain.reply.Reply;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class ReplySaveRequestDto {
+    private String replyName;
+    private String replyContent;
+    private Long replyBoardIdx; //외래키
+
+    @Builder
+    public ReplySaveRequestDto(String replyName, String replyContent,
+                               Long replyBoardIdx) {
+        this.replyName = replyName;
+        this.replyContent = replyContent;
+        this.replyBoardIdx = replyBoardIdx;
+    }
+    // DTO to Entity
+    public Reply toEntity(){
+        return Reply.builder()
+                .replyName(replyName)
+                .replyContent(replyContent)
+                .replyBoardIdx(replyBoardIdx)
+                .build();
+    }
+}
