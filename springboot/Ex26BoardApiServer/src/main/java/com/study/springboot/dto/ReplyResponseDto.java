67d1d1ac527729006d5e82928b7e36078de9bf92
zkaakakag <cscom222@naver.com>
2025-04-24 16:16:23 +0900

    댓글 응답 DTO생성 및 엔티티 변환 메서드 추가

---
+++ springboot/Ex26BoardApiServer/src/main/java/com/study/springboot/dto/ReplyResponseDto.java
@@ -0,0 +1,24 @@
+package com.study.springboot.dto;
+
+import com.study.springboot.domain.reply.Reply;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ReplyResponseDto {
+    private Long replyIdx;
+    private String replyName;
+    private String replyContent;
+    private LocalDateTime replyDate;
+    private Long boardIdx;
+
+    // Entity to DTO
+    public ReplyResponseDto(Reply entity){
+        this.replyIdx = entity.getReplyIdx();
+        this.replyContent = entity.getReplyContent();
+        this.replyName = entity.getReplyName();
+        this.replyDate = entity.getReplyDate();
+        this.boardIdx = entity.getBoard().getBoardIdx();
+    }
+}
