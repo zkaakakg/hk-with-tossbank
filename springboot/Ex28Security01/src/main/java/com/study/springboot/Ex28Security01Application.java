7566a549120a2bbf78298da7a72cf32cd5fe2fd1
zkaakakag <cscom222@naver.com>
2025-04-24 16:44:40 +0900

    Ex28Security01 프로젝트 생성

---
+++ springboot/Ex28Security01/src/main/java/com/study/springboot/Ex28Security01Application.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex28Security01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex28Security01Application.class, args);
+	}
+
+}
