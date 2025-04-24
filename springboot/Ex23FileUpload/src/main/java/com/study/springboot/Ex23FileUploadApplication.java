a54607fe10e8bd848b7a52dd1aa3a036e3b9f4b9
zkaakakag <cscom222@naver.com>
2025-04-24 09:30:20 +0900

    Ex23FileUpload 프로젝트 생성

---
+++ springboot/Ex23FileUpload/src/main/java/com/study/springboot/Ex23FileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex23FileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex23FileUploadApplication.class, args);
+	}
+
+}
