926d5f678b41f5a84b7a457a69e8a55e8320defd
zkaakakag <cscom222@naver.com>
2025-04-24 09:24:16 +0900

    애플리케이션에 Slf4j 기반 로깅 기능 초기 도입

--- springboot/Ex20Logger/src/main/java/com/study/springboot/Ex20LoggerApplication.java
+++ springboot/Ex20Logger/src/main/java/com/study/springboot/Ex20LoggerApplication.java
@@ -1,12 +1,15 @@
 package com.study.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class Ex20LoggerApplication {
 
 	public static void main(String[] args) {
+		log.info("test...");
 		SpringApplication.run(Ex20LoggerApplication.class, args);
 	}
 
