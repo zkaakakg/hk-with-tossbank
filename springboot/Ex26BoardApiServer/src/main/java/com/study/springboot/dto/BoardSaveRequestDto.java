97e2c33a3b8f528fbbd2b54c77f5ba427597f726
zkaakakag <cscom222@naver.com>
2025-04-24 16:16:04 +0900

    게시글 저장 요청을 위한 BoardSaveRequestDto 생성 및 엔티티 변환 메서드 추가

---
+++ springboot/Ex26BoardApiServer/src/main/java/com/study/springboot/dto/BoardSaveRequestDto.java
@@ -0,0 +1,34 @@
+package com.study.springboot.dto;
+
+import com.study.springboot.domain.board.Board;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class BoardSaveRequestDto {
+    private String boardName;
+    private String boardTitle;
+    private String boardContent;
+    private Long boardHit;
+
+    @Builder
+    public BoardSaveRequestDto(String boardName, String boardTitle,
+                            String boardContent, Long boardHit){
+        this.boardName = boardName;
+        this.boardTitle = boardTitle;
+        this.boardContent = boardContent;
+        this.boardHit = boardHit;
+    }
+    // DTO to Entity
+    public Board toEntity(){
+        return Board.builder()
+                .boardName(boardName)
+                .boardTitle(boardTitle)
+                .boardContent(boardContent)
+                .boardHit(boardHit)
+                .build();
+    }
+}
