5742da4e01d29314772e1ab338a1d451ca257b3f
zkaakakag <cscom222@naver.com>
2025-04-25 15:31:32 +0900

    SNS 사용자 정보 세션 저장

---
+++ springboot/Ex28Security04/src/main/java/com/study/springboot/service/SessionUser.java
@@ -0,0 +1,19 @@
+package com.study.springboot.service;
+
+import com.study.springboot.entity.SnsUser;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(SnsUser user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
